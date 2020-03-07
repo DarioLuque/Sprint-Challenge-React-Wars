ec08b3d06440ba3b06586f817642e6a3327b4e4f
Dario Luque <dario.lu92@gmail.com>
2020-03-07 09:43:39 -0800

    Created Components and installed dependencies

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard'
+
+function CharacterList () {
+    const [character, setCharacter] = useState([]);
+
+}
+
+export default CharacterList;
